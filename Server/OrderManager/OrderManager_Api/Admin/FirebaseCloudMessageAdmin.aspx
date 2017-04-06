d77a2b43ddf87cf4a3375ea73efca50400fc8551
ThanhVuongit2010 <thanhvuongit2010@gmail.com>
2017-04-06 11:04:36 +0700

    sss
    6/4/2017

---
+++ Server/OrderManager/OrderManager_Api/Admin/FirebaseCloudMessageAdmin.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FirebaseCloudMessageAdmin.aspx.cs" Inherits="OrderManager_Api.Admin.FirebaseCloudMessageAdmin" %>
+ 
+<!DOCTYPE html>
+ 
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<title></title>
+</head>
+<body>
+ 
+<form id="form1" runat="server">
+ 
+<div>
+Nhập compcode:
+<asp:TextBox ID="txtCompcode" runat="server" Width="326px"></asp:TextBox> 
+Nhập UserName:
+<asp:TextBox ID="txtUserName" runat="server" Width="326px"></asp:TextBox> 
+Nhập tiêu đề muốn gửi thông báo:
+<asp:TextBox ID="txtTieuDe" runat="server" Width="326px"></asp:TextBox>
+ 
+Nhập nội dung muốn gửi thông báo:
+<asp:TextBox ID="txtNoiDung" runat="server" Height="150px" TextMode="MultiLine" Width="358px"></asp:TextBox>
+ 
+<asp:Button ID="btnGui" runat="server" OnClick="btnGui_Click" Text="Gửi thông báo tới toàn bộ khách hàng" />
+ 
+kết quả sau khi gửi thông báo:
+<asp:TextBox ID="txtKetQua" runat="server" Height="150px" TextMode="MultiLine" Width="358px"></asp:TextBox>
+ 
+</div>
+ 
+</form>
+ 
+</body>
+</html>
---
+++ Server/OrderManager/OrderManager_Api/Admin/FirebaseCloudMessageAdmin.aspx.cs
@@ -0,0 +1,70 @@
+﻿using OrderManager_Api.Common;
+using OrderManager_Api.Controllers;
+using OrderManager_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OrderManager_Api.Admin
+{
+    public partial class FirebaseCloudMessageAdmin : System.Web.UI.Page
+    {
+        OrderManagerModelsDataContext db = new OrderManagerModelsDataContext();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnGui_Click(object sender, EventArgs e)
+        {
+            FCMController fcmController = new FCMController();
+            List<OD_USER> dsFcm = fcmController.TestPustFromSerVer(txtCompcode.Text, txtUserName.Text);
+            WebRequest tRequest;
+            //thiết lập FCM send
+            tRequest = WebRequest.Create(Constant.severFireCloudMessage);
+            tRequest.Method = "POST";
+            tRequest.UseDefaultCredentials = true;
+
+            tRequest.PreAuthenticate = true;
+
+            tRequest.Credentials = CredentialCache.DefaultNetworkCredentials;
+
+            //định dạng JSON
+            tRequest.ContentType = "application/json";
+            tRequest.Headers.Add(string.Format("Authorization: key={0}", Constant.serverkey));
+            tRequest.Headers.Add(string.Format("Sender: id={0}", Constant.senderID));
+
+            string[] arrRegid = dsFcm.Select(x => x.Device_Token).ToArray();
+            string RegArr = string.Empty;
+            RegArr = string.Join("\",\"", arrRegid);
+
+            string postData = "{ \"registration_ids\": [ \"" + RegArr + "\" ],\"data\": {\"message\": \"" + txtNoiDung.Text + "\",\"body\": \"" + txtNoiDung.Text + "\",\"title\": \"" + txtTieuDe.Text + "\",\"collapse_key\":\"" + txtNoiDung.Text + "\"}}";
+
+            Byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            tRequest.ContentLength = byteArray.Length;
+
+            Stream dataStream = tRequest.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse tResponse = tRequest.GetResponse();
+
+            dataStream = tResponse.GetResponseStream();
+
+            StreamReader tReader = new StreamReader(dataStream);
+
+            String sResponseFromServer = tReader.ReadToEnd();
+
+            txtKetQua.Text = sResponseFromServer; //Lấy thông báo kết quả từ FCM server.
+            tReader.Close();
+            dataStream.Close();
+            tResponse.Close();
+        }
+    }
+
+}
